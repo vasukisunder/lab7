544124cc4d113bb338baf836942243daf0e5a3ac
vasuki <sunder.vasuki@gmail.com>
2018-02-21 21:33:10 -0800

    lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,20 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(projectClick);
+
 	// your code here
+}
+
+
+
+function projectClick(e) { 
+
+ gtag('event', 'click', {'send_to': 'UA-114569744-1'});
+
+    // prevent the page from reloading      
+    e.preventDefault();
+
+
+    console.log("hello");
 }
